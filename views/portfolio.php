b2240da46bc4b2b79b0dcdbd45e2f370eb1769cc
Jiedara <m-vi@bulko.net>
2016-09-01 18:51:30 +0200

    add default image + portfolio video

--- views/portfolio.php
+++ views/portfolio.php
@@ -26,23 +26,63 @@
                     ===================================-->
                     <ul id='portfolio-grid' class="list-unstyled grid-portfolio">
 
-                      <li class="mix webdesign">
+                      <li class="mix vid">
                         <div class="item-portfolio">
-                          <a href="img/portfolio/big/pepperoni-273985_1920.jpg" class="preview-portfolio imgWrapper image-popup">
-                            <div class="mask"><div class="icn-center"><i class="fa fa-link "></i></div></div>
-                            <img src="img/portfolio/01.jpg" alt="" class="img-responsive"/>
+                          <a href="https://www.youtube.com/watch?v=2MNCUDoxmUQ" target="_blank" class="preview-portfolio">
+                            <div class="mask"><div class="icn-center"><i class="fa fa-play "></i></div></div>
+                            <img src="img/portfolio/default.jpg" alt="" class="img-responsive"/>
                           </a>
-                          <div class="desc-portfolio"><h5>PERSONAL  PROJECT <small>a Web Design</small></h5></div>
+                          <div class="desc-portfolio"><h5>Tu veux ma photo ?<small>Bande Annonce humorisitique, mélange de deux films avec Jim Carrey.</small></h5></div>
                         </div>
                       </li>
 
-                      <li class="mix webdesign">
+                      <li class="mix vid">
                         <div class="item-portfolio">
-                          <a href="img/portfolio/big/pepperoni-273985_1920.jpg" class="preview-portfolio imgWrapper image-popup">
-                            <div class="mask"><div class="icn-center"><i class="fa fa-link "></i></div></div>
-                            <img src="img/portfolio/01.jpg" alt="" class="img-responsive"/>
+                          <a href="https://www.youtube.com/watch?v=rKIwacjPVrE" target="_blank" class="preview-portfolio">
+                            <div class="mask"><div class="icn-center"><i class="fa fa-play "></i></div></div>
+                            <img src="img/portfolio/default.jpg" alt="" class="img-responsive"/>
                           </a>
-                          <div class="desc-portfolio"><h5>PERSONAL  PROJECT <small>a Web Design</small></h5></div>
+                          <div class="desc-portfolio"><h5>Le Dormeur Du Val <small>Illustration AudioVisuelle du poème d'Arthur Rimbaud</small></h5></div>
+                        </div>
+                      </li>
+
+                      <li class="mix vid">
+                        <div class="item-portfolio">
+                          <a href="http://webdoc.mmi-angouleme.fr/?webdocu=le-guide-des-internet" target="_blank" class="preview-portfolio">
+                            <div class="mask"><div class="icn-center"><i class="fa fa-play "></i></div></div>
+                            <img src="img/portfolio/default.jpg" alt="" class="img-responsive"/>
+                          </a>
+                          <div class="desc-portfolio"><h5>Le guide des Internet <small>Projet de fin d'étude, WebDocumentaire sur la culture du web</small></h5></div>
+                        </div>
+                      </li>
+
+                      <li class="mix vid">
+                        <div class="item-portfolio">
+                          <a href="https://www.youtube.com/watch?v=-zE4y_aB1zg" target="_blank" class="preview-portfolio">
+                            <div class="mask"><div class="icn-center"><i class="fa fa-play "></i></div></div>
+                            <img src="img/portfolio/default.jpg" alt="" class="img-responsive"/>
+                          </a>
+                          <div class="desc-portfolio"><h5>StopMorphing <small>Premier Court Métrage pour le concours 24h/24s, sur le thème de la Métamorphose</small></h5></div>
+                        </div>
+                      </li>
+
+                      <li class="mix vid dev">
+                        <div class="item-portfolio">
+                          <a href="https://www.youtube.com/watch?v=rcWffYFqxe8" target="_blank" class="preview-portfolio">
+                            <div class="mask"><div class="icn-center"><i class="fa fa-play "></i></div></div>
+                            <img src="img/portfolio/default.jpg" alt="" class="img-responsive"/>
+                          </a>
+                          <div class="desc-portfolio"><h5>La Petite Illusion <small>Second Court Métrage pour le concours 24h/24s, sur le thème de l'Illusion <br> Entièrement réalisé avec du CSS et jQuery</small></h5></div>
+                        </div>
+                      </li>
+
+                      <li class="mix vid">
+                        <div class="item-portfolio">
+                          <a href="https://www.youtube.com/watch?v=uTnajk9OT-4" target="_blank" class="preview-portfolio">
+                            <div class="mask"><div class="icn-center"><i class="fa fa-play "></i></div></div>
+                            <img src="img/portfolio/default.jpg" alt="" class="img-responsive"/>
+                          </a>
+                          <div class="desc-portfolio"><h5>Le Lit De Berty <small>Dernier Court Métrage pour le concours 24h/24s, sur le thème de la Liberté</small></h5></div>
                         </div>
                       </li>
                     </ul>
