ab2d626fba336f37925f729516ca15761fce0805
Martin Villanove <mvillanove@adneom.com>
2018-01-18 16:29:40 +0100

    add guloyana, remove compass

--- gulpfile.js
+++ gulpfile.js
@@ -1,41 +1,13 @@
-var gulp = require("gulp");
-var concat = require("gulp-concat");
-var plumber = require("gulp-plumber");
-var uglify = require("gulp-uglify");
-var compass = require('gulp-compass');
-var minifyCSS = require('gulp-clean-css');
-var git = require('gulp-git');
+var oyana = require('gulp-oyana');
 
-var jsPath = "./js";
-var cssPath = "./css";
-var scssPath = "./scss";
-var imgPath = "./img";
-
-gulp.task('default', ['script-concat', 'watch', 'compass']);
-
-gulp.task('script-concat', function () {
-
-	gulp.src([jsPath + '/script.js', '!' + jsPath + '/*.min.js'])
-		.pipe(plumber())
-		.pipe(concat('script.min.js'))
-		.pipe(uglify())
-		.pipe(gulp.dest(jsPath));
-});
-
-gulp.task('compass', function () {
-	gulp.src([scssPath + '/*.scss',scssPath + '/*/*.scss'])
-		.pipe(compass({
-			config_file: './config.rb',
-			css: cssPath,
-			sass: scssPath
-		}))
-		.pipe(plumber())
-		.pipe(minifyCSS())
-		.pipe(concat('style.min.css'))
-		.pipe(gulp.dest(cssPath));
-});
-
-gulp.task('watch', function () {
-	gulp.watch([jsPath + "/*.js", "!" + jsPath + "/*.min.js"], ['script-concat']);
-	gulp.watch([scssPath + "/*.scss",scssPath + "/*/*.scss"], ['compass']);
-});
+var options = {
+    "jsMinPath" : "./js",
+    "jsPath" : "./js",
+    "jsName" : "script.min",
+    "cssPath" : "./css",
+    "scssPath" : "./scss",
+    "cssName" : "style.min",
+    "outputStyle" : "compressed",
+    "proxyPath" : "http://localhost"
+}
+oyana( options );
